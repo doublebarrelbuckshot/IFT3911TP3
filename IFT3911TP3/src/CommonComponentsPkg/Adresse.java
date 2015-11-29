84488171be8e1d1c4459eb8ae5a2be90129c5f9c
Giancarlo Rizzi <giancarlo.rizzi@gmail.com>
2015-11-28 19:46:12 -0500

    client entity/person data updated

--- IFT3911TP3/src/CommonComponentsPkg/Adresse.java
+++ IFT3911TP3/src/CommonComponentsPkg/Adresse.java
@@ -1,6 +1,7 @@
 package CommonComponentsPkg;
 
 public class Adresse {
+	
 	private int _streetNumber;
 	private String _streetName;
 	private String _apartmentNumber;
@@ -8,4 +9,59 @@ public class Adresse {
 	private String _provinceState;
 	private String _country;
 	private String _postalZip;
+	public Adresse(int _streetNumber, String _streetName,
+			String _apartmentNumber, String _city, String _provinceState,
+			String _country, String _postalZip) {
+
+		this._streetNumber = _streetNumber;
+		this._streetName = _streetName;
+		this._apartmentNumber = _apartmentNumber;
+		this._city = _city;
+		this._provinceState = _provinceState;
+		this._country = _country;
+		this._postalZip = _postalZip;
+	}
+	public int get_streetNumber() {
+		return _streetNumber;
+	}
+	public void set_streetNumber(int _streetNumber) {
+		this._streetNumber = _streetNumber;
+	}
+	public String get_streetName() {
+		return _streetName;
+	}
+	public void set_streetName(String _streetName) {
+		this._streetName = _streetName;
+	}
+	public String get_apartmentNumber() {
+		return _apartmentNumber;
+	}
+	public void set_apartmentNumber(String _apartmentNumber) {
+		this._apartmentNumber = _apartmentNumber;
+	}
+	public String get_city() {
+		return _city;
+	}
+	public void set_city(String _city) {
+		this._city = _city;
+	}
+	public String get_provinceState() {
+		return _provinceState;
+	}
+	public void set_provinceState(String _provinceState) {
+		this._provinceState = _provinceState;
+	}
+	public String get_country() {
+		return _country;
+	}
+	public void set_country(String _country) {
+		this._country = _country;
+	}
+	public String get_postalZip() {
+		return _postalZip;
+	}
+	public void set_postalZip(String _postalZip) {
+		this._postalZip = _postalZip;
+	}
+	
 }
--- IFT3911TP3/src/ReservationPkg/Entity.java
+++ IFT3911TP3/src/ReservationPkg/Entity.java
@@ -3,13 +3,13 @@ package ReservationPkg;
 import CommonComponentsPkg.Adresse;
 
 public class Entity {
-	private int _numTel;
+	private String _numTel;
 	private Adresse _adresse;
 	private String _courriel;
 	public Entity(){}
 	
 	
-	public Entity(int _numTel, Adresse _adresse, String _courriel) {
+	public Entity(String _numTel, Adresse _adresse, String _courriel) {
 		super();
 		this._numTel = _numTel;
 		this._adresse = _adresse;
@@ -17,10 +17,10 @@ public class Entity {
 	}
 
 
-	public int get_numTel() {
+	public String get_numTel() {
 		return _numTel;
 	}
-	public void set_numTel(int _numTel) {
+	public void set_numTel(String _numTel) {
 		this._numTel = _numTel;
 	}
 	public Adresse get_adresse() {
